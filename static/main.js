ff5ee6061e450d1f7aa43a940b3a0bf9128de7cd
ivanvit <ivanvit100@gmail.com>
2023-11-09 13:32:59 +0300

    carousel

--- static/main.js
+++ static/main.js
@@ -39,13 +39,10 @@ function createCursor(){
     const cursor = document.createElement('div');
     cursor.classList.add('custom-cursor');
     document.body.appendChild(cursor);
-    document.addEventListener('mousemove', (e) => {
-        cursor.style.left = (e.clientX - 15) + 'px';
-        cursor.style.top = (e.clientY - 5) + 'px';
-    });
     let linkElements = document.querySelectorAll(".link");
     linkElements.forEach((element) => {
         element.addEventListener('mouseenter', () => {
+            document.querySelectorAll(".custom-cursor").length === 0 && createCursor();
             cursor.classList.add('scale-up');
         });
         element.addEventListener('mouseleave', () => {
@@ -62,21 +59,68 @@ function createCursor(){
         });
     });
 }
-document.addEventListener('DOMContentLoaded', () => {
-    if('ontouchstart' in window || (typeof DocumentTouch !== 'undefined' && document instanceof DocumentTouch))
-        document.querySelector("*").style.cursor = "auto !important";
-    else{
-        const handleLeave = () => {
-            document.querySelector(".custom-cursor").remove();
-        };
-        const handleEnter = () => {
-            createCursor();
-        };
-        document.body.addEventListener('mouseleave', handleLeave);
-        document.body.addEventListener('mouseenter', handleEnter);
-    }
-});
+const handleLeave = () => {
+    document.querySelector(".custom-cursor").remove();
+};
+const handleEnter = () => {
+    document.querySelectorAll(".custom-cursor").length === 0 && createCursor();
+};
+document.body.addEventListener('mouseleave', handleLeave);
+document.body.addEventListener('mouseenter', handleEnter);
 window.addEventListener('resize', changeSpanContent);
 let lastResFind;
 updateSearch();
 changeSpanContent();
+document.addEventListener('mousemove', (e) => {
+    const cursor = document.querySelectorAll(".custom-cursor");
+    cursor.length === 0 && createCursor();
+    try{
+        cursor[0].style.left = (e.clientX - 15) + 'px';
+        cursor[0].style.top = (e.clientY - 5) + 'px';
+    }catch(e){
+        console.warn("[cursor] Not found");
+    }
+});
+let tar = 0;
+var json = {
+    title: ['Базовый', 'Продвинутый'],
+    imgSrc: ['tar1', 'tar2'],
+    description: ['Готовая платформа на основе ранее созданных без добавления дополнительных функций на программном уровне по желанию заказчика', 
+    'Платформа на основе ранее созданных с добавлением или изменением базовых функций на программном уровне по желанию заказчика.']
+};
+function tarUpdate(){
+    console.log(tar);
+    document.querySelector("#item").innerHTML = `
+    <h1 class="textCur">${json.title[tar]}</h1>
+    <svg class="switch prev link" viewBox="0 0 110 110">
+        <path d="M90 7 L10 52 L90 97 Z" fill="none" stroke="black" stroke-width="10"/>
+    </svg>
+    <picture>
+        <source srcset="./static/images/${json.imgSrc[tar]}.webp" alt="Тариф" class="itemImg" height="200" width="200" type="image/webp">
+        <source srcset="./static/images/${json.imgSrc[tar]}.png" alt="Тариф" class="itemImg" height="200" width="200" type="image/png"> 
+        <img src="./static/images/${json.imgSrc[tar]}.png" alt="Тариф" class="itemImg">
+    </picture>
+    <svg class="switch next link" viewBox="0 0 110 110">
+        <path d="M10 7 L90 52 L10 97 Z" fill="none" stroke="black" stroke-width="10"/>
+    </svg>
+    <p class="textCur">${json.description[tar]}</p>`;
+    document.querySelector('.next').addEventListener('click', () => {
+        tar = tar < json.title.length ? tar + 1 : 0;
+        tarUpdate();
+    });
+    document.querySelector('.prev').addEventListener('click', () => {
+        tar = tar > 0 ? tar - 1 : json.title.length - 1; 
+        tarUpdate();    
+    });
+}
+document.addEventListener('DOMContentLoaded', () => {
+    createCursor();
+    document.querySelector('.next').addEventListener('click', () => {
+        tar = tar < json.title.length ? tar + 1 : 0;
+        tarUpdate();
+    });
+    document.querySelector('.prev').addEventListener('click', () => {
+        tar = tar > 0 ? tar - 1 : json.title.length - 1; 
+        tarUpdate();    
+    });
+});
