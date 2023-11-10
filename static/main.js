d093744f41de645814ed33def76522a29ad0beb4
ivanvit <ivanvit100@gmail.com>
2023-11-10 11:32:27 +0300

    carousl fix

--- static/main.js
+++ static/main.js
@@ -1,3 +1,6 @@
+const userAgent = navigator.userAgent.toLowerCase();
+const isMobile = /mobile|iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(userAgent);
+
 async function search(textToFind, lastResFind){
     event.preventDefault();
     const elements = document.querySelectorAll("span#search");
@@ -29,67 +32,89 @@ function updateSearch(){
         await search(document.querySelector('.search__input').value.trim().toLowerCase(), lastResFind);
     });
 }
+
 function changeSpanContent(){
     let spanElement = document.querySelector("#ap");
     spanElement.textContent = window.innerWidth < 340 ? 
         `ООО "АПЕЙРО"` :
         `Общество с ограниченной ответственностью "АПЕЙРО"`;
 }
+
 function createCursor(){
-    const cursor = document.createElement('div');
-    cursor.classList.add('custom-cursor');
-    document.body.appendChild(cursor);
-    let linkElements = document.querySelectorAll(".link");
-    linkElements.forEach((element) => {
-        element.addEventListener('mouseenter', () => {
-            document.querySelectorAll(".custom-cursor").length === 0 && createCursor();
-            cursor.classList.add('scale-up');
+    if(!isMobile){
+        const cursor = document.createElement('div');
+        cursor.classList.add('custom-cursor');
+        document.body.appendChild(cursor);
+        let linkElements = document.querySelectorAll(".link");
+        linkElements.forEach((element) => {
+            element.addEventListener('mouseenter', () => {
+                document.querySelectorAll(".custom-cursor").length === 0 && !isMobile && createCursor();
+                cursor.classList.add('scale-up');
+            });
+            element.addEventListener('mouseleave', () => {
+                cursor.classList.remove('scale-up');
+            });
         });
-        element.addEventListener('mouseleave', () => {
-            cursor.classList.remove('scale-up');
+        let textElements = document.querySelectorAll(".textCur");
+        textElements.forEach((element) => {
+            element.addEventListener('mouseenter', () => {
+                cursor.classList.add('text-cursor');
+            });
+            element.addEventListener('mouseleave', () => {
+                cursor.classList.remove('text-cursor');
+            });
         });
-    });
-    let textElements = document.querySelectorAll(".textCur");
-    textElements.forEach((element) => {
-        element.addEventListener('mouseenter', () => {
-            cursor.classList.add('text-cursor');
-        });
-        element.addEventListener('mouseleave', () => {
-            cursor.classList.remove('text-cursor');
-        });
-    });
+    }
 }
 const handleLeave = () => {
     document.querySelector(".custom-cursor").remove();
 };
 const handleEnter = () => {
-    document.querySelectorAll(".custom-cursor").length === 0 && createCursor();
+    document.querySelectorAll(".custom-cursor").length === 0 && !isMobile && createCursor();
 };
-document.body.addEventListener('mouseleave', handleLeave);
-document.body.addEventListener('mouseenter', handleEnter);
-window.addEventListener('resize', changeSpanContent);
+
+if(!isMobile){
+    document.body.addEventListener('mouseleave', handleLeave);
+    document.body.addEventListener('mouseenter', handleEnter);
+    window.addEventListener('resize', changeSpanContent);
+}
+
 let lastResFind;
 updateSearch();
 changeSpanContent();
 document.addEventListener('mousemove', (e) => {
     const cursor = document.querySelectorAll(".custom-cursor");
-    cursor.length === 0 && createCursor();
+    cursor.length === 0 && !isMobile && createCursor();
     try{
-        cursor[0].style.left = (e.clientX - 15) + 'px';
-        cursor[0].style.top = (e.clientY - 5) + 'px';
+        if(!isMobile){
+            cursor[0].style.left = (e.clientX - 15) + 'px';
+            cursor[0].style.top = (e.clientY - 5) + 'px';
+        }
     }catch(e){
         console.warn("[cursor] Not found");
     }
 });
+
 let tar = 0;
 var json = {
-    title: ['Базовый', 'Продвинутый'],
-    imgSrc: ['tar1', 'tar2'],
-    description: ['Готовая платформа на основе ранее созданных без добавления дополнительных функций на программном уровне по желанию заказчика', 
-    'Платформа на основе ранее созданных с добавлением или изменением базовых функций на программном уровне по желанию заказчика.']
+    title: ['Базовый', 'Продвинутый', 'Базовый'],
+    imgSrc: ['tar1', 'tar2', 'tar1'],
+    description: ['Готовая платформа на основе ранее созданных без добавления дополнительных функций на программном уровне по желанию заказчика.', 
+    'Платформа на основе ранее созданных с добавлением или изменением базовых функций на программном уровне по желанию заказчика.',
+    'Готовая платформа на основе ранее созданных без добавления дополнительных функций на программном уровне по желанию заказчика.']
 };
+
+function listener(){
+    document.querySelector('.next').addEventListener('click', () => {
+        tar = tar < 2 ? tar + 1 : 1;
+        tarUpdate();
+    });
+    document.querySelector('.prev').addEventListener('click', () => {
+        tar = tar > 0 ? tar - 1 : 1; 
+        tarUpdate();    
+    });
+}
 function tarUpdate(){
-    console.log(tar);
     document.querySelector("#item").innerHTML = `
     <h1 class="textCur">${json.title[tar]}</h1>
     <svg class="switch prev link" viewBox="0 0 110 110">
@@ -104,23 +129,8 @@ function tarUpdate(){
         <path d="M10 7 L90 52 L10 97 Z" fill="none" stroke="black" stroke-width="10"/>
     </svg>
     <p class="textCur">${json.description[tar]}</p>`;
-    document.querySelector('.next').addEventListener('click', () => {
-        tar = tar < json.title.length ? tar + 1 : 0;
-        tarUpdate();
-    });
-    document.querySelector('.prev').addEventListener('click', () => {
-        tar = tar > 0 ? tar - 1 : json.title.length - 1; 
-        tarUpdate();    
-    });
+    listener();
 }
-document.addEventListener('DOMContentLoaded', () => {
-    createCursor();
-    document.querySelector('.next').addEventListener('click', () => {
-        tar = tar < json.title.length ? tar + 1 : 0;
-        tarUpdate();
-    });
-    document.querySelector('.prev').addEventListener('click', () => {
-        tar = tar > 0 ? tar - 1 : json.title.length - 1; 
-        tarUpdate();    
-    });
-});
+
+!isMobile && createCursor();
+listener();
