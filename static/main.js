d7d3d5378092d72868b5cf955ef00b926a9c4b24
ivanvit <ivanvit100@gmail.com>
2023-11-14 21:22:57 +0300

    minify

--- static/main.js
+++ static/main.js
@@ -1,3 +1,4 @@
+"use strict";
 const userAgent = navigator.userAgent.toLowerCase();
 const isMobile = /mobile|iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(userAgent);
 const pref = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
